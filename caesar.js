af4c10034788a4d985a86dfa0597a28dde060630
Grisha <gm201212@gmail.com>
2021-05-05 22:10:10 +0300

    feat: add input.txt and output.txt files

--- caesar.js
+++ caesar.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
+const { option } = require('commander');
 const app = require('commander');
 const fs = require('fs');
 
 app
   .version('0.0.1')
   .option('-s --shift <shift>', 'add the shift for Caesar cipher')
-  .option('-i --input <input>', 'add an input file', './encoded.txt')
-  .option('-o --output <output>', 'add the output file', './decoded.txt')
+  .option('-i --input <input>', 'add an input file', './input.txt')
+  .option('-o --output <output>', 'add the output file', './output.txt')
   .option('-a --action <action>', 'add an action encode/decode')
   .action(options => {
     const { shift, input, output, action } = options;
+
+    console.log(options)
+    if (!shift || !action) {
+      process.stderr.write('The "shift" or "action" option is missed!\n');
+      process.exit(1);
+    }
+    
     fs.open(input, 'r', (err, fd) => {
       if (err) {
         if (err.code === "EEXIST") {
@@ -18,19 +26,13 @@ app
         } else {
           process.stderr.write('File doesn\'t exist or not accessible, please ensure that path is correct.\n');
         }
-      }
-      console.log(fd)
-    })
+      }  
+      console.log(fd)    
+      const readStream = fs.createReadStream(input, { encoding: 'utf-8', fd });
+      const writeStream = fs.createWriteStream(output);
+      readStream.pipe(writeStream);           
+    })   
     
-    console.log(shift)
-    if (!shift) {
-      console.error('The shift is missed!')
-      process.exit(1)
-    }
-    if (!action) {
-      console.error('The action is missed!')
-      process.exit(1)
-    }
   })
 
 app.parse();
