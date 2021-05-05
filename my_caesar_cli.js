5b4afe6a299c2520e1f18f44ceed0b11b7d0bc48
Grigory Murashka <gm201212@gmail.com>
2021-05-05 15:59:05 +0300

    feat: add error handling on non-existent file

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 const app = require('commander');
+const fs = require('fs');
+
 app
   .version('0.0.1')
   .option('-s --shift <shift>', 'add the shift for Caesar cipher')
-  .option('-i --input <input>', 'add an input file', './input.txt')
-  .option('-o --output <output>', 'add the output file', './output.txt')
+  .option('-i --input <input>', 'add an input file', './encoded.txt')
+  .option('-o --output <output>', 'add the output file', './decoded.txt')
   .option('-a --action <action>', 'add an action encode/decode')
   .action(options => {
     const { shift, input, output, action } = options;
+    fs.open(input, 'r', (err, fd) => {
+      if (err) {
+        if (err.code === "EEXIST") {
+          console.error('myfile already exists');
+          return;
+        } else {
+          process.stderr.write('File doesn\'t exist or not accessible, please ensure that path is correct.\n');
+        }
+      }
+      console.log(fd)
+    })
+    
     console.log(shift)
     if (!shift) {
       console.error('The shift is missed!')
