b88d2fa8647549ecd627f0efe0a4c71b46c92be3
Grigory Murashka <gm201212@gmail.com>
2021-05-06 14:39:37 +0300

    feat: add data recording to the console

--- caesar.js
+++ caesar.js
@@ -1,37 +1,48 @@
 #!/usr/bin/env node
-const { option } = require('commander');
 const app = require('commander');
 const fs = require('fs');
 
 app
   .version('0.0.1')
   .option('-s --shift <shift>', 'add the shift for Caesar cipher')
-  .option('-i --input <input>', 'add an input file', './input.txt')
-  .option('-o --output <output>', 'add the output file', './output.txt')
+  .option('-i --input <input>', 'add an input file')
+  .option('-o --output <output>', 'add the output file')
   .option('-a --action <action>', 'add an action encode/decode')
   .action(options => {
     const { shift, input, output, action } = options;
-
     console.log(options)
+
     if (!shift || !action) {
       process.stderr.write('The "shift" or "action" option is missed!\n');
       process.exit(1);
     }
-    
-    fs.open(input, 'r', (err, fd) => {
-      if (err) {
-        if (err.code === "EEXIST") {
-          console.error('myfile already exists');
-          return;
-        } else {
-          process.stderr.write('File doesn\'t exist or not accessible, please ensure that path is correct.\n');
+    if (!input) {
+      console.log(1000)
+      process.stdin.on('readable', () => {
+        const chunk = process.stdin.read();
+        if (chunk !== null) {
+          process.stdout.write(`data: ${chunk}`);            
         }
-      }  
-      console.log(fd)    
-      const readStream = fs.createReadStream(input, { encoding: 'utf-8', fd });
-      const writeStream = fs.createWriteStream(output);
-      readStream.pipe(writeStream);           
-    })   
+        process.stdin.resume();
+      }); 
+    } else {
+      fs.open(input, 'r', (err, fd) => {
+        if (err) {
+          if (err.code === "EEXIST") {
+            console.error('myfile already exists');
+            return;
+          } else {
+            process.stderr.write('File doesn\'t exist or not accessible, please ensure that path is correct.\n');
+            process.exit(1);
+          }
+        } else {
+          console.log(fd);
+          const readStream = fs.createReadStream(input, { encoding: 'utf-8', fd });
+          const writeStream = fs.createWriteStream(output);
+          readStream.pipe(writeStream); 
+        }         
+      })   
+    }    
     
   })
 
